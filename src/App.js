2eb16437db3593af1bba1d89966a2faae724f338
LeoLYW12138 <leo62227@gmail.com>
2022-06-17 23:35:30 +0800

    feat: add user page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import React from 'react'
 import {
   BrowserRouter as Router,
-  Switch,
-  Route,
   Redirect,
+  Route,
+  Switch,
 } from 'react-router-dom'
 
 import { Navbar } from './app/Navbar'
-import { PostsList } from './features/posts/PostsList'
 import { AddPostForm } from './features/posts/AddPostForm'
-import SinglePostPage from './features/posts/SinglePostPage'
 import EditPostForm from './features/posts/EditPostForm'
+import { PostsList } from './features/posts/PostsList'
+import SinglePostPage from './features/posts/SinglePostPage'
+import UserPage from './features/users/UserPage'
+import UsersList from './features/users/UsersList'
 
 function App() {
   return (
@@ -30,6 +31,8 @@ function App() {
           />
           <Route exact path="/posts/:postId" component={SinglePostPage} />
           <Route exact path="/editPost/:postId" component={EditPostForm} />
+          <Route exact path="/users" component={UsersList} />
+          <Route exact path="/users/:userId" component={UserPage} />
 
           <Redirect to="/" />
         </Switch>
